0ab30045642c3ebb3c0110170a6c6f7645236878
Mark Rondina <mark@rondina.net>
2016-08-15 14:23:02 -0400

    added API endpoint to indexjs file

--- index.js
+++ index.js
@@ -30,3 +30,18 @@ app.get('/webhook/', function(req, res) {
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function(req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for(let i=0; i < messaging_events.length; i++) {
+		let event = req.body.entry[0].messaging[i]
+		let sender = event.sender.id
+		if(event.message && event.message.text) {
+			let text = event.message.text
+			sendTextMessage(sender, "Yeah, boi! I hear you say: " + text.substring(0, 200))
+		}
+	}
+	res.sendStatus(200)
+})
+
+const token = "EAACRdZCXOqCIBABrn9QnRA360g0cX8gs6Dfa52R34DH60wZA5yklHvZBFYpkqZAGOr4mMxqBIRoKA0kHWv0lXzQBQ8zHFg7imJOFXTGQQEZAJC6ElEFGK7tcnFRLTSg8lZABZAysv38XpDwgVZA5Hy0CRtRlReawIELS6ZBb4o1dqoAZDZD"
