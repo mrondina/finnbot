0dce0723f4ce625f3bbef68fb8a3ba6b4dbec9d1
Mark Rondina <mark@rondina.net>
2016-08-19 11:14:53 -0400

    reconfiguring files to include the config files and all others to start building and debugging

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+'use strict'
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
+var Config = require('./config')
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.listen((process.env.PORT || 3000));
+app.listen(process.env.PORT || 3000);
 
 // Server frontpage
 app.get('/', function (req, res) {
