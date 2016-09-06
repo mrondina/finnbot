1ceee2ccbc7542a780bcac05a6ff69ee9f4853ea
Mark Rondina <mark@rondina.net>
2016-09-06 13:42:50 -0400

    fixing structure of wit processing in witjs

--- services/wit.js
+++ services/wit.js
@@ -1,116 +1,92 @@
 'use strict'
 
-var Config = require('../config')
-var FB = require('../connectors/facebook')
-var Wit = require('node-wit').Wit
-var request = require('request')
+var Config = require('../config');
+var FB = require('../connectors/facebook');
+var Wit = require('node-wit').Wit;
+var request = require('request');
 
 
 var firstEntityValue = function (entities, entity) {
 	var val = entities && entities[entity] &&
 		Array.isArray(entities[entity]) &&
 		entities[entity].length > 0 &&
-		entities[entity][0].value
+		entities[entity][0].value;
 
 	if (!val) {
-		return null
+		return null;
 	}
-	return typeof val === 'object' ? val.value : val
+	return typeof val === 'object' ? val.value : val;
 }
 
 
-var actions = {
-	say (sessionId, context, message, cb) {
-		// Bot testing mode, run cb() and return
-		if (require.main === module) {
-			cb()
-			return
-		}
+// Bot actions
+const actions = {
+  say(sessionId, context, message, cb) {
+    console.log(message);
 
-		console.log('WIT WANTS TO TALK TO:', context._fbid_)
-		console.log('WIT HAS SOMETHING TO SAY:', message)
-		console.log('WIT HAS A CONTEXT:', context)
+    // Bot testing mode, run cb() and return
+    if (require.main === module) {
+      cb();
+      return;
+    }
 
-		if (checkURL(message)) {
-			FB.newMessage(context._fbid_, message, true)
-		} else {
-			FB.newMessage(context._fbid_, message)
-		}
+    // Our bot has something to say!
+    // Let's retrieve the Facebook user whose session belongs to from context
+    // TODO: need to get Facebook user name
+    const recipientId = context._fbid_;
+    if (recipientId) {
+      // Yay, we found our recipient!
+      // Let's forward our bot response to her.
+      FB.fbMessage(recipientId, message, (err, data) => {
+        if (err) {
+          console.log(
+            'Oops! An error occurred while forwarding the response to',
+            recipientId,
+            ':',
+            err
+          );
+        }
 
-		
-		cb()
-		
+        // Let's give the wheel back to our bot
+        cb();
+      });
+    } else {
+      console.log('Oops! Couldn\'t find user in context:', context);
+      // Giving the wheel back to our bot
+      cb();
+    }
+  },
+  merge(sessionId, context, entities, message, cb) {
+    // Retrieve the location entity and store it into a context field
+    const cat = firstEntityValue(entities, 'category');
+    if (cat) {
+      context.cat = cat; // store it in context
+    }
+
+    cb(context);
+  },
+
+  error(sessionId, context, error) {
+    console.log(error.message);
+  },
+
+  // fetch-pics bot executes
+  ['fetch-pics'](sessionId, context, cb) {
+	var wantedPics = allPics[context.cat || 'default'];
+	context.pics = wantedPics[Math.floor(Math.random() * wantedPics.length)];
+	cb(context);
 	},
-
-	merge(sessionId, context, entities, message, cb) {
-		// Reset the weather story
-		delete context.forecast
-
-		// Retrive the location entity and store it in the context field
-		var loc = firstEntityValue(entities, 'location')
-		if (loc) {
-			context.loc = loc
-		}
-
-		// Reset the cutepics story
-		delete context.pics
-
-		// Retrieve the category
-		var category = firstEntityValue(entities, 'category')
-		if (category) {
-			context.cat = category
-		}
-
-		// Retrieve the sentiment
-		var sentiment = firstEntityValue(entities, 'sentiment')
-		if (sentiment) {
-			context.ack = sentiment === 'positive' ? 'Glad your liked it!' : 'Aww, that sucks.'
-		} else {
-			delete context.ack
-		}
-
-		cb(context)
-	},
-
-	error(sessionId, context, error) {
-		console.log(error.message)
-	},
-
-	// list of functions Wit.ai can execute
-	['fetch-weather'](sessionId, context, cb) {
-		// Here we can place an API call to a weather service
-		// if (context.loc) {
-		// 	getWeather(context.loc)
-		// 		.then(function (forecast) {
-		// 			context.forecast = forecast || 'sunny'
-		// 		})
-		// 		.catch(function (err) {
-		// 			console.log(err)
-		// 		})
-		// }
-
-		context.forecast = 'Sunny'
-
-		cb(context)
-	},
-
-	['fetch-pics'](sessionId, context, cb) {
-		var wantedPics = allPics[context.cat || 'default']
-		context.pics = wantedPics[Math.floor(Math.random() * wantedPics.length)]
-
-		cb(context)
-	},
-}
+};
 
 // SETUP THE WIT.AI SERVICE
 var getWit = function () {
 	console.log('GRABBING WIT')
 	return new Wit(Config.WIT_TOKEN, actions)
-}
+};
 
 module.exports = {
 	getWit: getWit,
-}
+};
 
 // BOT TESTING MODE
 if (require.main === module) {
@@ -119,20 +95,6 @@ if (require.main === module) {
 	client.interactive()
 }
 
-// GET WEATHER FROM API
-var getWeather = function (location) {
-	return new Promise(function (resolve, reject) {
-		var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+ location +'%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-		request(url, function (error, response, body) {
-		    if (!error && response.statusCode == 200) {
-		    	var jsonData = JSON.parse(body)
-		    	var forecast = jsonData.query.results.channel.item.forecast[0].text
-		      console.log('WEATHER API SAYS....', jsonData.query.results.channel.item.forecast[0].text)
-		      return forecast
-		    }
-			})
-	})
-}
 
 // CHECK IF URL IS AN IMAGE FILE
 var checkURL = function (url) {
